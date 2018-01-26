e18e8722333651f62ca3d8cb217215fe5b412831
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-01-26 08:13:04 -0500

    final file

---
+++ MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Biswash Adhikari on 1/25/18.
+//  Copyright © 2018 Biswash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
