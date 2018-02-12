d2601f5d4e51ca2e0023e971dae2ed8a42181e19
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-02-12 18:38:48 -0500

    new

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Biswash Adhikari on 2/12/18.
+//  Copyright © 2018 Biswash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
